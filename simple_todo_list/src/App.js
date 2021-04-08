138f667b7964ae8d039a70e7b332b97a4d90e27b
Marius <marius.vinogradovas@stud.vtmc.lt>
2021-04-08 20:22:40 +0300

    updates

--- simple_todo_list/src/App.js
+++ simple_todo_list/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 import TodoList from './components/TodoList';
 
-
 function App() {
   return (
     <div className="todo-app">
--- simple_todo_list/src/components/TodoList.js
+++ simple_todo_list/src/components/TodoList.js
@@ -29,8 +29,7 @@ function TodoList() {
 
     };
 
-
-
+    
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
             if (todo.id === id) {
